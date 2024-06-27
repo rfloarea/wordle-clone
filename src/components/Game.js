7280e97a9e12cfc022e0cffa4b65bc368ee58147
rfloarea <account@rfloarea.net>
2024-06-27 14:58:14 -0400

    correct import of `sample` util function

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { GuessInput } from "./GuessInput";
+import { WORDS } from "../data";
+import { sample } from "../utils";
+
+const answer = sample(WORDS);
+console.log(answer)
 
 export function Game() {
     const [gameState, setGameState] = React.useState();
 
     function handleGuessSubmit() {
-
+        console.log(`Guess: ${guess}. Answer: ${answer}`);
     }
 
     return (
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,19 @@
-// used to select a random word from our list
+// used to select a random word from our list words
 export const sample = (arr) => {
-    return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
 // a handy way to create a range of elements and things
 export const range = (start, end, step = 1) => {
-    let output = [];
-    if (typeof end === 'undefined') {
-        end = start;
-        start = 0;
-    }
-    for (let i = start; i < end; 1 += step) {
-        output.push(i);
-    }
-    return output;
+  let output = [];
+  if (typeof end === 'undefined') {
+    end = start;
+    start = 0;
+  }
+  for (let i = start; i < end; i += step) {
+    output.push(i);
+  }
+  return output;
 };
 
 /**
