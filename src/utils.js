330fbb79463193c0ab440c0e6d5a9886127971f2
rfloarea <account@rfloarea.net>
2024-06-27 10:45:28 -0400

    range and sample util functions

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+
+// used to select a random word from our list
+export const sample = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// a handy way to create a range of elements and things
+export const range = (start, end, step = 1) => {
+    let output = [];
+    if (typeof end === 'undefined') {
+        end = start;
+        start = 0;
+    }
+    for (let i = start; i < end; 1 += step) {
+        output.push(i);
+    }
+    return output;
+};
