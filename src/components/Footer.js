36c5010f5fa32c7a8d73e083b70af58965307b41
rfloarea <account@rfloarea.net>
2024-07-03 12:13:06 -0400

    footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p>Made for you, by me</p>
+      <p>Made for you, by me.</p>
       <p>Find the code <a href="https://github.com/rfloarea/wordle-clone" >here</a>.</p>
     </footer>
   );
