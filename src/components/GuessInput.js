233d49fd870434ffe0db82fa7ecbc591cf0fd5fb
rfloarea <account@rfloarea.net>
2024-07-03 11:10:39 -0400

    clean up

--- src/components/GuessInput.js
+++ src/components/GuessInput.js
@@ -1,43 +1,36 @@
 import React from 'react';
 
-/** This component is/controls the input field where the player submits their word
- * It takes two arguments:
- * 1. gameState, which is used here to disable the input field if the game is over
- * 2. handletentativeGuessSubmit, which is used here to send our player's tentativeGuess to the Game component (which holds all the game logic)
- * There is some client-side validation which checks for:
- * 1. If the tentativeGuess is exactly 5 characters
- * 2. If the tentativeGuess is only letters
- * 
- */
+function GuessInput({ gameStatus, handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
 
-export function GuessInput({ gameState, handleGuessSubmit }) {
-    const [tentativeGuess, setTentativeGuess] = React.useState('');
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    function handleSubmit(ev) {
-        ev.preventDefault();
-        handleGuessSubmit(tentativeGuess);
-        setTentativeGuess('');
-    }
+    handleSubmitGuess(tentativeGuess);
 
-    return (
-        <form onSubmit={handleSubmit} className='guess-input-wrapper'>
-            <label htmlFor='guess-input'>Enter guess:</label>
-            <input
-                id='guess-input'
-                type='text'
-                value={tentativeGuess}
-                onChange={(event) => {
-                    const nextGuess = event.target.value.toUpperCase();
-                    setTentativeGuess(nextGuess);
-                }}
-                required
-                minLength={5}
-                maxLength={5}
-                pattern='[a-zA-Z]{5}'
-                title='5 letter words only'
-                disabled={gameState !== 'running'}
+    setTentativeGuess('');
+  }
 
-            />
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        disabled={gameStatus !== 'running'}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={tentativeGuess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+      />
+    </form>
+  );
 }
+
+export default GuessInput;
