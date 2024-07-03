819f7b78c1e4303891ad7ded23da2b1be50a7246
rfloarea <account@rfloarea.net>
2024-07-03 11:13:26 -0400

    clean up

--- src/components/GuessResults.js
+++ src/components/GuessResults.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { NUM_OF_GUESSES } from "../constants";
-import { range } from "../utils";
+import React from 'react';
 
-export function GuessResults({ guesses }) {
-    return (
-        <>
-        {range(NUM_OF_GUESSES).map((guess, index) => (
-            <p key={index}>{guesses[index]}</p>
-        ))}
-        </>
-    );
+import { NUM_OF_GUESSES_ALLOWED } from '../constants';
+import { range } from '../utils';
+
+import Guess from './Guess';
+
+function GuessResults({ validatedGuesses }) {
+  return (
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={validatedGuesses[num]} />
+      ))}
+    </div>
+  );
 }
+
+export default GuessResults;
