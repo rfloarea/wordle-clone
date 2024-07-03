1460eebfe56ac7490c97b0009b18ae963c76f865
rfloarea <account@rfloarea.net>
2024-07-03 11:18:45 -0400

    clean up and prep for future components

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,29 +1,68 @@
-import React from "react";
-import { GuessInput } from "./GuessInput";
-import { GuessResults } from "./GuessResults";
-import { WORDS } from "../data";
-import { sample } from "../utils";
+import React from 'react';
 
-const answer = sample(WORDS);
-console.log(answer)
+import { sample } from '../utils';
+import { WORDS } from '../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../constants';
+import { checkGuess } from '../game-helpers';
 
-export function Game() {
-    // running | won | lost
-    const [gameState, setGameState] = React.useState('running');
-    const [guesses, setGuesses] = React.useState('');
+import GuessInput from './GuessInput';
+import GuessResults from './GuessResults';
 
-    function handleGuessSubmit(tentativeGuess) {
-        console.log(`Guess: ${tentativeGuess}. Answer: ${answer}`);
-        setGuesses(tentativeGuess)
+import WonBanner from './WonBanner';
+import LostBanner from './LostBanner';
+import Keyboard from './Keyboard';
+
+
+function Game() {
+  const [answer, setAnswer] = React.useState(() => sample(WORDS));
+
+  // running | won | lost
+  const [gameStatus, setGameStatus] = React.useState('running');
+
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    const nextGuesses = [...guesses, tentativeGuess];
+    setGuesses(nextGuesses);
+
+    if (tentativeGuess.toUpperCase() === answer) {
+      setGameStatus('won');
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lost');
     }
+  }
 
-    return (
-        <>
-            <GuessResults guesses={guesses}/>
-            <GuessInput 
-                handleGuessSubmit={handleGuessSubmit} 
-                gameState={gameState}
-            />
-        </>
-    );
+  function handleRestart() {
+    const newAnswer = sample(WORDS);
+    setAnswer(newAnswer);
+    setGuesses([]);
+    setGameStatus('running');
+  }
+
+  const validatedGuesses = guesses.map((guess) =>
+    checkGuess(guess, answer)
+  );
+
+  return (
+    <>
+      <GuessResults validatedGuesses={validatedGuesses} />
+      <GuessInput
+        gameStatus={gameStatus}
+        handleSubmitGuess={handleSubmitGuess}
+      />
+      <Keyboard validatedGuesses={validatedGuesses} />
+
+      {gameStatus === 'won' && (
+        <WonBanner
+          numOfGuesses={guesses.length}
+          handleRestart={handleRestart}
+        />
+      )}
+      {gameStatus === 'lost' && (
+        <LostBanner answer={answer} handleRestart={handleRestart} />
+      )}
+    </>
+  );
 }
+
+export default Game;
